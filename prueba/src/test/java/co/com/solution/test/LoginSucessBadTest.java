e4bc6825a918f9d4945f24040f1a7620e035f2b2
Cristian Mira <cmira0906@gmail.com>
2018-03-13 12:07:12 -0500

    Se valida el título de la página

--- prueba/src/test/java/co/com/solution/test/LoginSucessBadTest.java
+++ prueba/src/test/java/co/com/solution/test/LoginSucessBadTest.java
@@ -1,5 +1,6 @@
 package co.com.solution.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import co.com.solution.common.BaseClass;
@@ -9,6 +10,10 @@ public class LoginSucessBadTest extends BaseClass{
 	@Test
 	public void loginSucces() throws Exception{
 		driver.get("http://sahitest.com/demo/training/login.htm");
+		
+		String titulo= driver.getTitle();
+		Assert.assertEquals("Login", titulo);
+		System.out.println("Titulo de la pg: "+titulo);
 	
 		
 	}
